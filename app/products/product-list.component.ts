4477be15522fc270dda5527288c5bdb087cc29f1
Paulo Ferrari <paulooferrari@gmail.com>
2016-05-22 22:04:32 -0300

    services and dependency injection

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from 'angular2/core';
 import { IProduct } from './product';
 import { ProductFilterPipe } from './product-filter.pipe';
 import { StarComponent } from '../shared/star.component';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -16,35 +17,18 @@ export class ProductListComponent implements OnInit{
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description" : "awdaiodjaiowjdoiajwdiojwaiodj adaw",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl" : "http://www.lojadomecanico.com.br//mostra_foto.asp?foto=2/253/83527/Martelo-de-Unha-de-27mm-com-Cabo-de-Made-fortgpro-fg86301.JPG&largura=450&altura=450&d=08/04/2015085039"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "GDN-0048",
-            "releaseDate": "March 12, 2016",
-            "description" : "xxx jaiowjdoiajwdiojwaiodj adaw",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl" : "http://www.lojadomecanico.com.br//mostra_foto.asp?foto=2/253/83527/Martelo-de-Unha-de-27mm-com-Cabo-de-Made-fortgpro-fg86301.JPG&largura=450&altura=450&d=08/04/2015085039"
-        }
-    ];
+    products: IProduct[];
+    
+    constructor(private _productService: ProductService){
+        
+    }
     
     toggleImage(): void{
         this.showImage = !this.showImage;
     }
     
     ngOnInit(): void{
-        
+        this.products = this._productService.getProducts();
     }
     
    onRatingClicked(message: string): void {
